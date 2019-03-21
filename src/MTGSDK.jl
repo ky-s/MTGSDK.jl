6463029e31399f7a42ce43974348f66a5fb46494
ky-s <kyohei_86@hotmail.com>
2019-03-22 00:02:20 +0900

    レスポンス整形実装

--- src/MTGSDK.jl
+++ src/MTGSDK.jl
@@ -1,6 +1,8 @@
 module MTGSDK
 
-using HTTP: get
+using HTTP: get, Response
+using JSON: parse
+using Pipe
 
 export request, cards, sets, booster, types, subtypes, supertypes, formats
 
@@ -17,8 +19,18 @@ function _build_url(resource::AbstractString; id=nothing, query=nothing, raw=not
     return url
 end
 
+function _parse(resp::Response, take)
+    resp.status == 200 && return (resp.body |> String |> parse |> take)
+    resp.status == 400 && return "400: We could not process that action"
+    resp.status == 403 && return "403: You exceeded the rate limit"
+    resp.status == 404 && return "404: The requested resource could not be found"
+    resp.status == 500 && return "500: We had a problem with our server. Please try again later"
+    resp.status == 503 && return "503: We are temporarily offline for maintenance. Please try again later"
+    "$(resp.status): undefined HTTP status. ->($resp)"
+end
+
 """
-    request(resource; <keyword arguments>) -> HTTP.Response
+    request(resource; <keyword arguments>) -> Dict{String,Any}
 
 request to any resource.
 
@@ -27,10 +39,10 @@ example
 request("cards", id=386616)
 ```
 """
-request(resource::AbstractString; kw...) = get(_build_url(resource; kw...))
+request(resource::AbstractString; take=(r)->r[resource], kw...) = @pipe _build_url(resource; kw...) |> get |> _parse(_, take)
 
 """
-    cards([id]; <keyword arguments>) -> HTTP.Response
+    cards([id]; <keyword arguments>) -> Dict{String,Any}
 
 request to cards and cards/:id resource.
 
@@ -42,11 +54,11 @@ cards(id=386616)
 cards(query=Dict("supertypes" => "legendary", "types" => "creature", "color" => "red"))
 ```
 """
-cards(; kw...)   = request("cards"; kw...)
-cards(id; kw...) = request("cards"; id=id, kw...)
+cards(; id=nothing, kw...)   = id === nothing ? request("cards"; kw...) : cards(id; kw...)
+cards(id; kw...) = request("cards"; id=id, take=(r)->r["card"], kw...)
 
 """
-    sets([set]; <keyword arguments>) -> HTTP.Response
+    sets([set]; <keyword arguments>) -> Dict{String,Any}
 
 request to sets and sets/:id resource.
 
@@ -59,10 +71,10 @@ sets(query=Dict("supertypes" => "legendary", "types" => "creature", "color" => "
 ```
 """
 sets(; kw...)  = request("sets"; kw...)
-sets(set; kw...)  = request("sets"; id=set, kw...)
+sets(set; kw...)  = request("sets"; id=set, take=(r)->r["set"], kw...)
 
 """
-    booster(set; <keyword arguments>) -> HTTP.Response
+    booster(set; <keyword arguments>) -> Dict{String,Any}
 
 request to sets/:id/booster resource.
 
@@ -71,31 +83,31 @@ example
 booster("rna")
 ```
 """
-booster(set) = request("sets"; id=set, raw="/booster")
+booster(set) = request("sets"; id=set, raw="/booster", take=(r)->r["cards"])
 
 """
-    types(; <keyword arguments>) -> HTTP.Response
+    types(; <keyword arguments>) -> Dict{String,Any}
 
 request to types resource.
 """
 types() = request("types")
 
 """
-    subtypes(; <keyword arguments>) -> HTTP.Response
+    subtypes(; <keyword arguments>) -> Dict{String,Any}
 
 request to subtypes resource.
 """
 subtypes() = request("subtypes")
 
 """
-    supertypes(; <keyword arguments>) -> HTTP.Response
+    supertypes(; <keyword arguments>) -> Dict{String,Any}
 
 request to supertypes resource.
 """
 supertypes() = request("supertypes")
 
 """
-    formats(; <keyword arguments>) -> HTTP.Response
+    formats(; <keyword arguments>) -> Dict{String,Any}
 
 request to formats resource.
 """
--- test/sdk.jl
+++ test/sdk.jl
@@ -6,104 +6,60 @@ using JSON
 using .MTGSDK
 
 @testset "_build_url" begin
-    @test MTGSDK._build_url("cards") == "$(MTGSDK.ROOT)/$(MTGSDK.VERSION)/cards"
-    @test MTGSDK._build_url("cards", id=65535) == "$(MTGSDK.ROOT)/$(MTGSDK.VERSION)/cards/65535"
-    @test MTGSDK._build_url("cards", query=Dict("name" => "khans", "color" => "red,black")) == "$(MTGSDK.ROOT)/$(MTGSDK.VERSION)/cards?name=khans&color=red,black"
-    @test MTGSDK._build_url("sets", raw="?page=2&pageSize=10") == "$(MTGSDK.ROOT)/$(MTGSDK.VERSION)/sets?page=2&pageSize=10"
+    baseurl = "$(MTGSDK.ROOT)/$(MTGSDK.VERSION)"
+    @test MTGSDK._build_url("cards") == "$baseurl/cards"
+    @test MTGSDK._build_url("cards", id=65535) == "$baseurl/cards/65535"
+    @test MTGSDK._build_url("cards", query=Dict("name" => "khans", "color" => "red,black")) == "$baseurl/cards?name=khans&color=red,black"
+    @test MTGSDK._build_url("sets", raw="?page=2&pageSize=10") == "$baseurl/sets?page=2&pageSize=10"
     @test MTGSDK._build_url("cards", version="v2") == "$(MTGSDK.ROOT)/v2/cards"
 end
 
-@testset "request status:ok" begin
-    HTTP.get(url) = HTTP.Response(JSON.json(Dict("url" => url))) # mock
-
-    resp = MTGSDK.request("cards")
-    @test resp.status == 200
-    @test JSON.parse(String(resp.body)) == Dict("url" => "$(MTGSDK.ROOT)/$(MTGSDK.VERSION)/cards")
-end
-
-@testset "request status:not found" begin
-    error = Dict("error" => "Error: Not Found")
-    HTTP.get(url) = HTTP.Response(404, JSON.json(error)) # mock
-
-    resp = MTGSDK.request("nothing")
-    @test resp.status == 404
-    @test JSON.parse(String(resp.body)) == error
+@testset "_parse" begin
+    @test MTGSDK._parse(HTTP.Response(200, JSON.json(Dict("take" => "taken"))), (r) -> r["take"]) == "taken"
+    @test match(r"^400:", MTGSDK._parse(HTTP.Response(400, ""), +)) !== nothing
+    @test match(r"^403:", MTGSDK._parse(HTTP.Response(403, ""), +)) !== nothing
+    @test match(r"^404:", MTGSDK._parse(HTTP.Response(404, ""), +)) !== nothing
+    @test match(r"^500:", MTGSDK._parse(HTTP.Response(500, ""), +)) !== nothing
+    @test match(r"^503:", MTGSDK._parse(HTTP.Response(503, ""), +)) !== nothing
+    @test match(r"^418:", MTGSDK._parse(HTTP.Response(418, "I'm a tea pot."), +)) !== nothing
 end
 
 @testset "cards" begin
     cards = Dict("cards" => [Dict("name" => "Black Lotus"), Dict("name" => "Mox Ruby")])
     HTTP.get(url) = HTTP.Response(JSON.json(cards)) # mock
+    @test MTGSDK.cards() == cards["cards"]
+    @test MTGSDK.cards(query=Dict("name" => "khans", "color" => "red,black")) == cards["cards"]
 
-    resp = MTGSDK.cards()
-    @test resp.status == 200
-    @test JSON.parse(String(resp.body)) == cards
-end
-
-@testset "cards(id)" begin
-    HTTP.get(url) = HTTP.Response(JSON.json(Dict("url" => url))) # mock
-
-    resp = MTGSDK.cards(1234)
-    @test resp.status == 200
-    @test JSON.parse(String(resp.body)) == Dict("url" => "$(MTGSDK.ROOT)/$(MTGSDK.VERSION)/cards/1234")
-
-    resp = MTGSDK.cards(id=5678)
-    @test resp.status == 200
-    @test JSON.parse(String(resp.body)) == Dict("url" => "$(MTGSDK.ROOT)/$(MTGSDK.VERSION)/cards/5678")
-end
-
-@testset "cards(query)" begin
-    HTTP.get(url) = HTTP.Response(JSON.json(Dict("url" => url))) # mock
-
-    resp = MTGSDK.cards(query=Dict("name" => "khans", "color" => "red,black"))
-    @test resp.status == 200
-    # TODO: もしかしたら Dict の順番は保証されないかも。そうすると URL 中の name と color の順番は入れ替わるかもしれない。
-    @test JSON.parse(String(resp.body)) == Dict("url" => "$(MTGSDK.ROOT)/$(MTGSDK.VERSION)/cards?name=khans&color=red,black")
+    card = Dict("card" => Dict("name" => "Black Lotus"))
+    HTTP.get(url) = HTTP.Response(JSON.json(card)) # mock
+    @test MTGSDK.cards(1234) == card["card"]
+    @test MTGSDK.cards(id=1234) == card["card"]
 end
 
 @testset "sets" begin
-    HTTP.get(url) = HTTP.Response(JSON.json(Dict("url" => url))) # mock
+    sets = Dict("sets" => [Dict("name" => "Alpha"), Dict("name" => "Beta")])
+    HTTP.get(url) = HTTP.Response(JSON.json(sets)) # mock
 
-    resp = MTGSDK.sets()
-    @test resp.status == 200
-    @test JSON.parse(String(resp.body)) == Dict("url" => "$(MTGSDK.ROOT)/$(MTGSDK.VERSION)/sets")
+    @test MTGSDK.sets() == sets["sets"]
+    @test MTGSDK.sets(query=Dict("page" => "2", "pageSize" => "10")) == sets["sets"]
+
+    set = Dict("set" => Dict("name" => "Alpha"))
+    HTTP.get(url) = HTTP.Response(JSON.json(set)) # mock
+    @test MTGSDK.sets("rna") == set["set"]
 end
 
-@testset "sets(set)" begin
-    HTTP.get(url) = HTTP.Response(JSON.json(Dict("url" => url))) # mock
+@testset "booster" begin
+    cards = Dict("cards" => [Dict("name" => "Black Lotus"), Dict("name" => "Mox Ruby")])
+    HTTP.get(url) = HTTP.Response(JSON.json(cards))
 
-    resp = MTGSDK.sets("rna")
-    @test resp.status == 200
-    @test JSON.parse(String(resp.body)) == Dict("url" => "$(MTGSDK.ROOT)/$(MTGSDK.VERSION)/sets/rna")
-end
-
-@testset "sets(query)" begin
-    HTTP.get(url) = HTTP.Response(JSON.json(Dict("url" => url))) # mock
-
-    resp = MTGSDK.sets(query=Dict("page" => "2", "pageSize" => "10"))
-    @test resp.status == 200
-    @test JSON.parse(String(resp.body)) == Dict("url" => "$(MTGSDK.ROOT)/$(MTGSDK.VERSION)/sets?pageSize=10&page=2")
+    @test MTGSDK.booster("ktk") == cards["cards"]
 end
 
 @testset "other resouces" begin
-    HTTP.get(url) = HTTP.Response(JSON.json(Dict("url" => url))) # mock
+    HTTP.get(url) = HTTP.Response(JSON.json( reduce(merge, map(r->Dict(r=>r),("types", "subtypes", "supertypes", "formats"))) ))
 
-    resp = MTGSDK.types()
-    @test resp.status == 200
-    @test JSON.parse(String(resp.body)) == Dict("url" => "$(MTGSDK.ROOT)/$(MTGSDK.VERSION)/types")
-
-    resp = MTGSDK.subtypes()
-    @test resp.status == 200
-    @test JSON.parse(String(resp.body)) == Dict("url" => "$(MTGSDK.ROOT)/$(MTGSDK.VERSION)/subtypes")
-
-    resp = MTGSDK.supertypes()
-    @test resp.status == 200
-    @test JSON.parse(String(resp.body)) == Dict("url" => "$(MTGSDK.ROOT)/$(MTGSDK.VERSION)/supertypes")
-
-    resp = MTGSDK.formats()
-    @test resp.status == 200
-    @test JSON.parse(String(resp.body)) == Dict("url" => "$(MTGSDK.ROOT)/$(MTGSDK.VERSION)/formats")
-
-    resp = MTGSDK.booster("rna")
-    @test resp.status == 200
-    @test JSON.parse(String(resp.body)) == Dict("url" => "$(MTGSDK.ROOT)/$(MTGSDK.VERSION)/sets/rna/booster")
+    @test MTGSDK.types()      == "types"
+    @test MTGSDK.subtypes()   == "subtypes"
+    @test MTGSDK.supertypes() == "supertypes"
+    @test MTGSDK.formats()    == "formats"
 end
